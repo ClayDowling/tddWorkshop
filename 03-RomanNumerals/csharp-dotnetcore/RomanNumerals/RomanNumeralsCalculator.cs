96a5c293b889fcc5567d94b3e71576c35a6f9d4f
todd flanders <toddfbass@gmail.com>
2021-07-07 08:20:39 -0400

    start table look up

--- 03-RomanNumerals/csharp-dotnetcore/RomanNumerals/RomanNumeralsCalculator.cs
+++ 03-RomanNumerals/csharp-dotnetcore/RomanNumerals/RomanNumeralsCalculator.cs
@@ -1,16 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Katas
 {
     public class Roman
     {
+        private static Dictionary<string, int> romanValues = new Dictionary<string, int>
+        {
+            {"L", 50},
+            {"X", 10},
+            {"IX", 9},
+            {"V", 5},
+            {"IV", 4},
+            {"I", 1},
+        };
+        
         static void Main(string[] args)
         {
         }
 
-        public static int ToInt(string numeral) {
+        public static int ToInt(string numeral)
+        {
+            var result = 0;
             
-            return 0;
+            while (numeral.StartsWith("L"))
+            {
+                result += romanValues["L"];
+                numeral = numeral.Substring("L".Length);
+            }
+            
+            while (numeral.StartsWith("X"))
+            {
+                result += 10;
+                numeral = numeral.Substring(1);
+            }
+            
+            if (numeral.StartsWith("IX"))
+            {
+                result += 9;
+                numeral = numeral.Substring(2);
+            }
+            
+            if (numeral.StartsWith("V"))
+            {
+                result += 5;
+                numeral = numeral.Substring(1);
+            }
+
+            if (numeral.StartsWith("IV"))
+            {
+                result += 4;
+                numeral = numeral.Substring(2);
+            }
+
+            return result + numeral.Length;
         }
 
         public static string Add(string numeral1, string numeral2)
@@ -19,5 +62,11 @@ namespace Katas
             return numeral1 == "XV" ? "XX" : 
                 $"{numeral1}{numeral2}";
         }
+    }
+
+    internal class RomanArabicToken
+    {
+        public string Roman { get; set; }
+        public int Arabic { get; set; }
     }
 }
