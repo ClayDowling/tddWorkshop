588667110c28b8328e571b729fcfe4a8ca6e9c02
todd flanders <toddfbass@gmail.com>
2021-07-08 11:03:07 -0400

    CoinSensors is now an array

--- 08-VendingMachine/csharp-dotnetcore/VendingMachine/CoinAccepter.cs
+++ 08-VendingMachine/csharp-dotnetcore/VendingMachine/CoinAccepter.cs
@@ -6,24 +6,19 @@ namespace VendingMachine
 {
     public class CoinAccepter
     {
-        public Gpio Coin1; 
-        public Gpio Coin2; 
-        public Gpio Coin3; 
-        public Gpio Coin4;
+        public readonly Gpio[] CoinSensors = new Gpio[4];
         private List<CoinSensor> _coinSensorConfig;
 
         public void Setup(string denomination)
         {
-            Coin1 = new Gpio(13);
-            Coin2 = new Gpio(14);
-            Coin3 = new Gpio(15);
-            Coin4 = new Gpio(16);
-            
-            using (StreamReader r = new StreamReader($"configuration/{denomination}.json"))
-            {
-                var json = r.ReadToEnd();
-                _coinSensorConfig = JsonConvert.DeserializeObject<List<CoinSensor>>(json);
-            }            
+            CoinSensors[0] = new Gpio(13);
+            CoinSensors[1] = new Gpio(14);
+            CoinSensors[2] = new Gpio(15);
+            CoinSensors[3] = new Gpio(16);
+
+            using var r = new StreamReader($"configuration/{denomination}.json");
+            var json = r.ReadToEnd();
+            _coinSensorConfig = JsonConvert.DeserializeObject<List<CoinSensor>>(json);
         }
         
         public void Loop()
@@ -42,15 +37,8 @@ namespace VendingMachine
                     break;
                 }
             }
-            switch (sensor)
-            {
-                case 1:
-                    Coin1.State = GpioState.HIGH;
-                    break;
-                case 2:
-                    Coin2.State = GpioState.HIGH;
-                    break;
-            };
+
+            CoinSensors[sensor-1].State = GpioState.HIGH;
         }
     }
 }
--- 08-VendingMachine/csharp-dotnetcore/VendingMachineTests/CoinAccepterTest.cs
+++ 08-VendingMachine/csharp-dotnetcore/VendingMachineTests/CoinAccepterTest.cs
@@ -18,17 +18,17 @@ namespace VendingMachineTests
             _accepter = new CoinAccepter();
             _accepter.Setup("UsaCoins");
             _cashDropped = 0;
-            _accepter.Coin1.AttachInterrupt(ProcessNickle, GpioState.HIGH);
-            _accepter.Coin2.AttachInterrupt(ProcessDime, GpioState.HIGH);
+            _accepter.CoinSensors[0].AttachInterrupt(ProcessNickle, GpioState.HIGH);
+            _accepter.CoinSensors[1].AttachInterrupt(ProcessDime, GpioState.HIGH);
         }
         
         [Fact]
         public void Setup_byDefault_InitializesCoinGpiosToPins13to16()
         {
-            _accepter.Coin1.Pin.Should().Be(13);
-            _accepter.Coin2.Pin.Should().Be(14);
-            _accepter.Coin3.Pin.Should().Be(15);
-            _accepter.Coin4.Pin.Should().Be(16);
+            _accepter.CoinSensors[0].Pin.Should().Be(13);
+            _accepter.CoinSensors[1].Pin.Should().Be(14);
+            _accepter.CoinSensors[2].Pin.Should().Be(15);
+            _accepter.CoinSensors[3].Pin.Should().Be(16);
         }
         
         // TODO: allow for different GPIO wiring
