be9f679967a8384f552017eea2022b201098179f
todd flanders <toddfbass@gmail.com>
2021-07-08 14:08:38 -0400

    first pass at coin accepter

--- 08-VendingMachine/csharp-dotnetcore/VendingMachine/CoinAccepter.cs
+++ 08-VendingMachine/csharp-dotnetcore/VendingMachine/CoinAccepter.cs
@@ -1,17 +1,65 @@
-﻿namespace VendingMachine
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
 {
     public class CoinAccepter
     {
-        public Gpio Coin1; 
-        
-        public void Setup()
+        private int _value;
+
+        private static List<CoinSpecification> coinSpecs = new List<CoinSpecification>
         {
-            Coin1 = new Gpio(-1);
-        }
-        
-        public void Loop()
+            new CoinSpecification(5.000, 21.21, 5),
+            new CoinSpecification(2.268, 17.91, 10),
+            new CoinSpecification(5.670, 24.26, 25),
+            new CoinSpecification(8, 30, 100)
+        };
+
+        public void DropCoin(double weightGrams, double diameterMm)
         {
-            
+            _value += GetValueForCoin(weightGrams, diameterMm);
         }
+
+        private int GetValueForCoin(double weightGrams, double diameterMm)
+        {
+            foreach (var spec in coinSpecs)
+            {
+                if (InTolerance(spec, weightGrams, diameterMm))
+                {
+                    return spec.Value;
+                }
+            }
+            return 0;
+        }
+
+        private bool InTolerance(CoinSpecification spec, double weightGrams, double diameterMm)
+        {
+            if (spec.Weight - 0.001 < weightGrams && weightGrams < spec.Weight + 0.001
+                                                  && spec.DiameterMm - 0.001 < diameterMm &&
+                                                  diameterMm < spec.DiameterMm + 0.001)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int Value()
+        {
+            return _value;
+        }
+    }
+
+    internal class CoinSpecification
+    {
+        public CoinSpecification(double weight, double diameterMm, int value)
+        {
+            Weight = weight;
+            DiameterMm = diameterMm;
+            Value = value;
+        }
+
+        public double Weight { get; set; }
+        public double DiameterMm { get; set; }
+        public int Value { get; set; }
     }
 }
--- 08-VendingMachine/csharp-dotnetcore/VendingMachineTests/CoinAccepterTest.cs
+++ 08-VendingMachine/csharp-dotnetcore/VendingMachineTests/CoinAccepterTest.cs
@@ -7,6 +7,14 @@ namespace VendingMachineTests
 {
     public class CoinAccepterTest
     {
+        private const double NickelWeight = 5.000;
+        private const double NickelDiameter = 21.21;
+        private const double DimeWeight = 2.268;
+        private const double DimeDiameter = 17.91;
+        private const double QuarterWeight = 5.670;
+        private const double QuarterDiameter = 24.26;
+        private const int MagicTokenWeight = 8;
+        private const int MagicCoinDiameter = 30;
         private CoinAccepter _accepter;
 
         public CoinAccepterTest()
@@ -14,12 +22,34 @@ namespace VendingMachineTests
             _accepter = new CoinAccepter();
         }
         
-        [Fact]
-        public void Setup_byDefault_InitializesCoin1GpioToPin13()
+        [Theory]
+        [InlineData("Nickel", NickelWeight, NickelDiameter, 5)]
+        [InlineData("Dime", DimeWeight, DimeDiameter, 10)]
+        [InlineData("Quarter", QuarterWeight, QuarterDiameter, 25)]
+        [InlineData("MagicToken", MagicTokenWeight, MagicCoinDiameter, 100)]
+        public void AcceptsUsaCoins(string name, double weightGrams, double diameterMm, int expectValueCents)
         {
-            _accepter.Setup();
-            
-            _accepter.Coin1.Pin.Should().Be(13);
+            _accepter.DropCoin(weightGrams, diameterMm);
+            _accepter.Value().Should().Be(expectValueCents);
+        }
+
+        [Fact]
+        public void RejectsSlug()
+        {
+            _accepter.DropCoin(5.7, 18);
+            _accepter.Value().Should().Be(0);
+            // TODO: verify coin return?
+        }
+
+        [Fact]
+        public void AccumulatesCoins()
+        {
+            _accepter.DropCoin(NickelWeight, NickelDiameter);
+            _accepter.DropCoin(NickelWeight, NickelDiameter);
+            _accepter.DropCoin(DimeWeight, DimeDiameter);
+            _accepter.DropCoin(QuarterWeight, QuarterDiameter);
+
+            _accepter.Value().Should().Be(45);
         }
     }
 }
